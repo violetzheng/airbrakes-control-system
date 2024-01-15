c33873555c0ea1d952dc83d7eea9f8ed19ebf618
User <violet.ywz@gmail.com>
2024-01-15 20:46:24 +1100

    more changes

---
+++ sim_the_model.m
@@ -0,0 +1,3 @@
+si = Simulink.SimulationInput('ver1_3DOF_draft2');
+
+if  ~isnan(args.StopTime)
---
+++ wrapper_for_simulink.m
@@ -0,0 +1,20 @@
+clear
+clc
+close all
+
+current_time = 0;
+current_height = 0;
+current_vel = 0;
+current_pitch = 0;
+
+sim('ver1_3DOF_draft2.slx');
+heightSignal = ans.yout.getElement('height');
+velocitySignal = ans.yout.getElement('velocity');
+t = heightSignal.Values.Time;
+height = heightSignal.Values.Data;
+velocity = velocitySignal.Values.Data;
+
+figure
+plot(t,height)
+plot(t,velocity)
+
