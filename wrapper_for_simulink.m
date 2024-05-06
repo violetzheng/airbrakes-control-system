ac107353e698e59316d2915969ea3235176a219f
User <violet.ywz@gmail.com>
2024-05-06 18:05:34 +1000

    serp2 stuff

--- wrapper_for_simulink.m
+++ wrapper_for_simulink.m
@@ -2,10 +2,11 @@ clear
 clc
 close all
 
-current_height = 0;
-current_vel = 0;
-current_pitch = 0;
-extension_length = 0;
+current_height = 0.0;
+current_vel = 0.0;
+current_pitch = 0.0;
+extension_length = 0.0;
+StartTime = 0; 
 
 sim('ver1_3DOF_draft2.slx');
 heightSignal = ans.yout.getElement('height');
@@ -14,7 +15,8 @@ t = heightSignal.Values.Time;
 height = heightSignal.Values.Data;
 velocity = velocitySignal.Values.Data;
 
-figure
-plot(t,height)
-%plot(t,velocity)
+
+
+
+
 
